25c5c9e1cb5d62fca26dd008257930decdc901fd
LiamTheNinja5 <50884811+LiamTheNinja5@users.noreply.github.com>
2023-03-27 19:16:13 -0400

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,3 +4,4 @@ export const UPDATE_PERIOD_MILLIS = 100
 export const SECONDS_SPENT_BLINKING = 5;
 export const AMOUNT_OF_BLINKING = 11
 export const ANIMATION_DEFAULT_PERCENTAGE = 1/3
+export const DEFAULT_URL = "https://rentry.org/AFIP2023/raw"; // if no "#jsonobject" is set in the URL params
